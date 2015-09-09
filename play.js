a10454786222d8ca76ed96d9a66bf788ab8169b6
jerryliuoft <jerryliuoft@gmail.com>
2015-09-09 12:26:59 -0400

    added bird movement

--- play.js
+++ play.js
@@ -3,18 +3,33 @@
 var PlayState = {
 	create: function (){
 		this.game.physics.startSystem(Phaser.Physics.ARCADE);
-		this.game.physics.arcade.gravity.y = 500;
+		this.game.physics.arcade.gravity.y = 1200;
 
 		//add the background sprite
 		this.background = this.game.add.sprite (0,0,'background');
 
 		this.bird = Bird(100, this.game.height/2);
+		this.bird.flap = function(){
+			this.body.velocity.y = - 400;
+
+		}
 
 		this.ground = this.game.add.tileSprite(0,400,335,112,'ground');
 		this.ground.autoScroll(-200,0);
 		this.game.physics.arcade.enableBody(this.ground);
 		this.ground.body.allowGravity = false;
 		this.ground.body.immovable = true;
+
+
+		// keep the spacebar from propogating up to the browser
+		this.game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
+		//add keyboard controls
+		var flapKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		flapKey.onDown.add(this.bird.flap, this.bird);
+
+		//add mouse/touch controls
+
+		this.input.onDown.add(this.bird.flap, this.bird);
 	},
 	update:function (){
 		this.game.physics.arcade.collide(this.bird, this.ground);
